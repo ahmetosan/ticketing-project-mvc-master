0f17616ff94128f1420210171212904e3151eae4
ahmetosan <ahmetpcf@gmail.com>
2022-03-24 21:08:07 -0400

    CRUD Service Created

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -5,18 +5,20 @@ import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
-public interface RoleService {
+public interface RoleService extends CrudService<RoleDTO,Long>{
 
     //save
     //findById
     //delete
     //findAll
 
+/*
     RoleDTO save(RoleDTO role);
     RoleDTO findById(Long id);
     List<RoleDTO> findAll();
     void delete(RoleDTO role);
     void deleteById(Long id);
 
+*/
 
 }
